2adab5a628080cbc3f7d21142f54c8d487108291
unknown <mrehawk82@gmail.com>
2021-12-23 18:56:47 -0500

    rollback

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-// URL
-const url = "https://google.com";
-
-// pass url to fetch() method
-// attach a then() handler
-fetch(url)
-  .then((res) => {
-    return res.text();
-  })
-  .then((html) => {
-    console.log(html); // We will get our converted HTML plain text
-  });
+console.log("body");
