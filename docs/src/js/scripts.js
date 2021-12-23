7c1c588c73ac079b2d4f812bac908f1eac438eb2
unknown <mrehawk82@gmail.com>
2021-12-23 16:16:19 -0500

    npm init

---
+++ docs/src/js/scripts.js
@@ -0,0 +1,15 @@
+var init = () => {
+	buildUI();
+},
+buildUI = () => {
+	var container = createEle("div");
+
+	container.innerHTML = "hello container";
+	container.className = "container w3-container w3-black";
+
+	body.append(container);
+};
+
+window.onload = () => {
+	init();
+};
