ad7ca1f37726921b705d341792966ae80af6f787
unknown <mrehawk82@gmail.com>
2021-12-24 13:12:26 -0500

    module fetch

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("body");
+import fetch from 'node-fetch';
+
+const response = await fetch('https://www.google.com');
+const r1 = await response.clone();
+
+const results = await Promise.all([response.json(), r1.text()]);
+
+console.log(results[795]);
