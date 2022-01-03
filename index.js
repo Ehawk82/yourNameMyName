3fa89f284182f6951c82721cc66cf9093d514139
unknown <mrehawk82@gmail.com>
2022-01-03 09:20:11 -0500

    async function wrap

--- index.js
+++ index.js
@@ -1,69 +1,74 @@
 import fetch from 'node-fetch';
 
+//
 
+const surnameCount = async (x,y) => { 
+  let input1 = x,
+      input2 = y;
 
-let input1 = "graziano",
-    input2 = "peters";
+  //
 
+  const response1 = await fetch('https://www.names.org/n/'+input1+'/about#');
+  const data1 = await response1.text();
+  const param1_data = data1.substring(
+            data1.lastIndexOf("the U.S. Census Bureau surveyed") + 32,
+            data1.lastIndexOf("people with the last name")
+          );
 
+  const response2 = await fetch('https://www.names.org/n/'+input2+'/about#');
+  const data2 = await response2.text();
+  const param2_data = data2.substring(
+            data2.lastIndexOf("the U.S. Census Bureau surveyed") + 32,
+            data2.lastIndexOf("people with the last name")
+          );
+  const p1d = parseFloat(param1_data.replace(/,/g, '')),
+        p2d = parseFloat(param2_data.replace(/,/g, ''));
 
-const response1 = await fetch('https://www.names.org/n/'+input1+'/about#');
-const data1 = await response1.text();
-const param1_data = data1.substring(
-          data1.lastIndexOf("the U.S. Census Bureau surveyed") + 32,
-          data1.lastIndexOf("people with the last name")
-        );
-const response2 = await fetch('https://www.names.org/n/'+input2+'/about#');
-const data2 = await response2.text();
-const param2_data = data2.substring(
-          data2.lastIndexOf("the U.S. Census Bureau surveyed") + 32,
-          data2.lastIndexOf("people with the last name")
-        );
-const p1d = parseFloat(param1_data.replace(/,/g, '')),
-      p2d = parseFloat(param2_data.replace(/,/g, ''));
+  //
 
+  let result1 = p1d;
+  let result2 = p2d;
 
+  //
 
-let result1 = p1d;
-let result2 = p2d;
+  var diff = (a, b) => { return Math.abs(a - b); };
 
+  //
 
+  let results = diff(result1,result2);
+  if(input1 === ""){
+    input1 = "no name entered";
+  }
+  if(input2 === ""){
+    input2 = "no name entered";
+  }
+  if(Number.isNaN(result1)){
+     result1 = "there are no results for that name";
+  };
+  if(Number.isNaN(result2)){
+     result2 = "there are no results for that name";
+  };
+  if(Number.isNaN(results)){
+     results = "cannot calculate";
+  }
 
-var diff = (a, b) => { return Math.abs(a - b); };
+  //
 
-
-
-let results = diff(result1,result2);
-if(input1 === ""){
-  input1 = "no name entered";
+  var r1 = (i,r) => {
+    console.log(i + ": " + r);
+  };
+  var r2 = (i,r) => {
+    console.log(i + ": " + r);
+  };
+  var rs = r => {
+    console.log(r);
+  };
+  r1(input1,result1);
+  r2(input2,result2);
+  rs(results);
 }
-if(input2 === ""){
-  input2 = "no name entered";
-}
-if(Number.isNaN(result1)){
-   result1 = "there are no results for that name";
-};
-if(Number.isNaN(result2)){
-   result2 = "there are no results for that name";
-};
-if(Number.isNaN(results)){
-   results = "cannot calculate";
-}
+//
 
 
 
-var r1 = (i,r) => {
-  console.log(i + ": " + r);
-};
-var r2 = (i,r) => {
-  console.log(i + ": " + r);
-};
-var rs = r => {
-  console.log(r);
-};
-
-
-
-r1(input1,result1);
-r2(input2,result2);
-rs(results);
+surnameCount("graziano", "cassel");
